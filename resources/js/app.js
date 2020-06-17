f5efa406c4677e0f4e8ae0a6d9da5a62b41426bf
Shahram Keshavarz <ic18b058@technikum-wien.at>
2020-06-17 16:33:24 +0200

    changed app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,7 +28,7 @@ Vue.component('create-spell', require('./components/CreateSpellComponent.vue').d
 Vue.component('create-kind', require('./components/CreateKindComponent.vue').default);
 
 Vue.component('edit-spell', require('./components/EditSpellComponent.vue').default);
-Vue.component('edit-spells', require('./components/EditSpell.vue').default);
+Vue.component('edit-kind', require('./components/EditKindComponent.vue').default);
 
 Vue.component('list-spells', require('./components/ListSpellsComponent.vue').default);
 Vue.component('list-kinds', require('./components/ListKindsComponent.vue').default);
